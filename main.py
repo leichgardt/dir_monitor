38fcc98105340ce140b502adbfbc588cb5cdf11d
leichgardt <TooEz3325948448_>
2022-09-02 01:39:13 +0300

    Added frontend

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-"""
-Нужно реализовать монитор содержимого каталога.
-Есть сервер и клиент. После запуска сервер следит за изменениями в заданном каталоге.
-Если содержимое изменилось, оно должно быстро обновиться на клиенте.
-Сервер должен каким-то образом получать путь к каталогу (например, через ключ запуска
-или конфиг).
-Клиент и сервер работают независимо
-
-Код может запускаться в докере или просто консоли. Клиентское приложение может быть страничкой в браузере.
-"""
 import json
 import os
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from aioredis.exceptions import ConnectionError as RedisConnectionError
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.templating import Jinja2Templates
 from websockets.exceptions import ConnectionClosedOK
 
 from src.cache_engine import RedisEngine
-from src.logger import Logger
+from src.file_model import File
+from src.logger import logger
+
+
+__author__ = 'Leichgardt'
 
 
 REDIS_CHANNEL = os.environ.get('REDIS_FW_CHANNEL', 'file_watcher')
 REDIS_URL = os.environ.get('REDIS_FW_URL', 'redis://localhost/0')
 
 app = FastAPI()
+template = Jinja2Templates(directory='templates')
 cache = RedisEngine(REDIS_URL)
-logger = Logger.with_default_handlers()
+
+
+@app.on_event('startup')
+async def server_startup():
+    cache.connect_to_server()
+
+
+@app.on_event('shutdown')
+async def server_startup():
+    await cache.close()
+
+
+@app.get('/')
+async def index(request: Request):
+    return template.TemplateResponse('index.html', context={'request': request})
 
 
 @app.websocket('/ws')
@@ -31,15 +42,23 @@ async def websocket_endpoint(websocket: WebSocket):
     pubsub = cache.get_pubsub()
     try:
         await websocket.accept()
-        await websocket.send_json({'data': await cache.get_all()})
+        data = await cache.get_all()
+        sorted_data = []
+        for file, time in sorted(data.items(), key=lambda item: item[1]):
+            sorted_data.append(File(file=file, data={'status': 'new', 'time': time}).to_obj())
+        await websocket.send_json(sorted_data)
         await pubsub.subscribe(REDIS_CHANNEL)
         while True:
             message = await pubsub.get_message(ignore_subscribe_messages=True)
             if message:
-                await websocket.send_json(json.loads(message).get('data'))
+                data = json.loads(message['data'].decode('utf-8'))
+                await websocket.send_json(data)
     except (WebSocketDisconnect, ConnectionClosedOK):
         pass
+    except RedisConnectionError as e:
+        logger.error(f'Redis PubSub connection error on Websocket: {e}')
     except Exception as e:
-        await logger.exception(f'WebSocket error: {e}')
+        logger.exception(f'WebSocket error: {e}')
     finally:
         await pubsub.unsubscribe()
+        await websocket.close()
